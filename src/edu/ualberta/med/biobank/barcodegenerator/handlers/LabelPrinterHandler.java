be2a0a8b714a21bb68342346f22f168d99cbca51
Nelson Loyola <loyola@cs.ualberta.ca>
2011-06-02 15:28:15 -0600

    barcodeGenerator has its own perspective now

--- src/edu/ualberta/med/biobank/barcodegenerator/handlers/LabelPrinterHandler.java
+++ src/edu/ualberta/med/biobank/barcodegenerator/handlers/LabelPrinterHandler.java
@@ -6,27 +6,33 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.WorkbenchException;
 
 import edu.ualberta.med.biobank.barcodegenerator.Activator;
+import edu.ualberta.med.biobank.barcodegenerator.perspective.MainPerspective;
 import edu.ualberta.med.biobank.barcodegenerator.views.LabelPrinterView;
 
 public class LabelPrinterHandler extends AbstractHandler implements IHandler {
 
-	public static final String ID = "edu.ualberta.med.biobank.barcodegenerator.handlers.LabelPrinterHandler";
+    public static final String ID = "edu.ualberta.med.biobank.barcodegenerator.handlers.LabelPrinterHandler";
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbench workbench = Activator.getDefault().getWorkbench();
-		IWorkbenchPage page = workbench.getActiveWorkbenchWindow()
-				.getActivePage();
-		try {
-			page.showView(LabelPrinterView.ID);
-		} catch (PartInitException e) {
-			throw new ExecutionException(
-					"Could not open label printer view : ", e);
-		}
-		return null;
-	}
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbench workbench = Activator.getDefault().getWorkbench();
+        try {
+            if (workbench.getActiveWorkbenchWindow().getActivePage()
+                .closeAllEditors(true)) {
+                workbench.showPerspective(MainPerspective.ID,
+                    workbench.getActiveWorkbenchWindow());
+                IWorkbenchPage page = workbench.getActiveWorkbenchWindow()
+                    .getActivePage();
+                page.showView(LabelPrinterView.ID);
+            }
+        } catch (WorkbenchException e) {
+            throw new ExecutionException(
+                "Could not open label printer view : ", e);
+        }
+        return null;
+    }
 
 }
